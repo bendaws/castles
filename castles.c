aafd1f4b9e6e3857588dd5bcc29ddabb7f89cbdf
Ben <ben@linuxfornoobs.org>
2024-06-21 17:19:02 -0500

    working board logic

--- castles.c
+++ castles.c
@@ -21,9 +21,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <stdio.h>
+#define clrscr() printf("\e[1;1H\e[2J")
+#endif
+
 #define CASTLES_VERSION "13.0.0-beta"
 #define CASTLES_UPDATED "21 Jun 2024 (09:05 / 9:05AM)"
 #define CASTLES_LICENSE "GNU General Public License (Version 3)"
+
+int board[20][20] = {{}, {}};
+
+char* typeconv(int identifier) {
+    switch (identifier) {
+        case 0:
+            return " ";
+            break;
+        case 1:
+            return "#";
+            break;
+        case 2:
+            return "O";
+            break;
+        case 3:
+            return "X";
+            break;
+        default:
+            return " ";
+            break;
+    }
+}
+
+int find(int px, int py) {
+    return board[px][py];
+}
+
+void place(int px, int py, int state) {
+    board[px - 1][py - 1] = state;
+}
 
 char* strappend(char* text, char* symbol) {
     char* appendednl = malloc(strlen(text) + 2); strcpy(appendednl, text); strcat(appendednl, symbol);
@@ -64,6 +101,26 @@ void version() {
     println("under certain conditions.");
 }
 
+void drawBoard(int target[20][20]) {
+    // A 2D array is required for this
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            printf(typeconv(target[j][i]));
+            if (j == 19) {
+                printf("\n");
+            }
+        }
+    }
+};
+
+void clearBoard(int target[20][20]) {
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            target[i][j] = 0;
+        }
+    }
+}
+
 // currently working on a complete rewrite (v13)
 
 int main(int argc, char* argv[]) {
@@ -72,8 +129,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    if (strcmp(argv[1], "--version") == 0) {
-        version();
-        return 0;
-    }
+    clearBoard(board);
+    place(1, 1, 3);
+    place(1, 2, 3);
+    place(1, 3, 3);
+    place(1, 4, 3);
+    place(1, 5, 3);
+    drawBoard(board);
 }
